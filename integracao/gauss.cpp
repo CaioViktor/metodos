52448c5ff36ac251b8dbb50f36a46cf84389fcd2
CaioViktor <caioviktor@hotmail.com>
2015-03-25 13:06:04 -0300

    Começo das outra gradraturas de gauss

--- integracao/gauss.cpp
+++ integracao/gauss.cpp
@@ -74,6 +74,98 @@ double gaussLegendre(int n,double a,double b){
 		i+= gE(a,b,e[c]) * w[c];
 	return i * (b-a)/2 ;
 }
+
+
+double gaussHermite(int n,double a,double b){
+	double e[n],w[n],i = 0;
+	switch(n){
+		case 2:
+			e[0] = 0.707;
+			w[0] = 0.88622;
+
+			e[1] = -0.707;
+			w[1] = 0.88622;
+		break;
+
+		case 3:
+			e[0] = 0;
+			w[0] = 1.18163590;
+
+			e[1] = 1.22474487;
+			w[1] = 0.29540897;
+
+			e[2] = -1.22474487;
+			w[2] = 0.29540897;
+		break;
+
+		case 4:
+			e[0] = 0.52464762;
+			w[0] = 0.80491409;
+
+			e[1] = -0.52464762;
+			w[1] = 0.80491409;
+
+			e[2] = 1.65068012;
+			w[2] = 0.08131283;
+
+			e[3] = -1.65068012;
+			w[3] = 0.08131283;
+		break;
+	}
+	for(int c = 0 ; c < n; c++)
+		i+= f(e[c]) * w[c];//TODO: usar f(xi) = physicists' Hermite polynomials
+	return i;
+}
+
+double gaussLaguerre(int n,double a,double b){
+	double e[n],w[n],i = 0;
+	switch(n){
+		case 2:
+			e[0] = 0.58578643;
+			w[0] = 0.85355339;
+
+			e[1] = 3.41421356;
+			w[1] = 0.14644660;
+		break;
+
+		case 3:
+			e[0] = 0.41577455;
+			w[0] = 0.71109300;
+
+			e[1] = 2.24428036;
+			w[1] = 0.27851973;
+
+			e[2] = 6.28994508;
+			w[2] = 0.01038926;
+		break;
+
+		case 4:
+			e[0] = 0.32254768;
+			w[0] = 0.60315410;
+
+			e[1] = 1.74576110;
+			w[1] = 0.35741869;
+
+			e[2] = 4.53662029;
+			w[2] = 0.03888791;
+
+			e[3] = 9.39507091;
+			w[3] = 0.00053929;
+		break;
+	}
+	for(int c = 0 ; c < n; c++)
+		i+= f(e[c]) * w[c];// TODO f(x) = f(xi) = polinômio de laguerre
+	return i;
+}
+
+double gaussChebyshev(int n,double a,double b){
+	double i = 0;
+	for(int k = 0 ; k <= n; k++)
+		i+= f(cos((2*k -1)*(M_PI/n) )); 
+	return (M_PI/n)*i;
+}
+
+
 double gaussLegendreError(int j,double error,double a, double b){
 	int n = 1,v = 1;
 	double dif,ij = 0,ik;
--- integracao/gauss.h
+++ integracao/gauss.h
@@ -1,3 +1,6 @@
 double gaussLegendre(int n,double a,double b);
 double gaussLegendreError(int j,double error,double a, double b);
 double gE(double a, double b, double e);
+double gaussHermite(int n,double a,double b);
+double gaussLaguerre(int n,double a,double b);
+double gaussChebyshev(int n,double a,double b);
